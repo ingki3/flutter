e6e2d2652d9dc9f540dc99f112748d20348ef051
ingki3 <ingki3@gmail.com>
2019-09-16 14:56:01 +0900

    20190916_7

---
+++ inflearnapp/lib/cupertino_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class CupertinoPage extends StatefulWidget {
+  @override
+  _CupertinoPageState createState() => _CupertinoPageState();
+}
+
+class _CupertinoPageState extends State<CupertinoPage> {
+  bool _switch = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text('Cupertino UI'),
+      ),
+      body: Column(children: <Widget>[
+        CupertinoButton(
+          child: Text('Cupertino Button'),
+          onPressed: () {},
+          ),
+        CupertinoSwitch(
+          value: _switch,
+          onChanged: (bool value) {
+            setState(() {
+              _switch = value;
+            });
+          },
+          )
+        ],
+      ),
+    );
+  }
+}
--- inflearnapp/lib/main.dart
+++ inflearnapp/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'cupertino_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +11,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+//    home: CupertinoPage(),
       home: MyAppStateful(title: 'Hello World'),
     );
   }
 }
-
 
 class MyAppStateful extends StatefulWidget {
   final String title;
@@ -37,6 +30,7 @@ class MyAppStateful extends StatefulWidget {
 
 class _MyAppStatefulState extends State<MyAppStateful> {
   String _message = 'Hello World';
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,13 +40,26 @@ class _MyAppStatefulState extends State<MyAppStateful> {
       appBar : AppBar(
         title: Text(widget.title),
       ),
-      body: Text(_message, style: TextStyle(fontSize: 30))
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children : <Widget>[
+            Text(_message, style: TextStyle(fontSize: 30)),
+            Text('$_counter', style: TextStyle(fontSize: 30)),
+            RaisedButton(
+              child: Text('Move page'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => CupertinoPage()));
+              },
+            )
+          ], ))
     );
   }
         
   void _changeMessage() {
     setState(() {
       _message = 'Welcome to my app';
+      _counter ++;
     });
   }
 }
