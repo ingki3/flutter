1ee2186001e899cd4cd4ff9645ad9efb574e4c86
ingki3 <ingki3@gmail.com>
2019-09-19 16:35:04 +0900

    20190919_1

--- instagram_clone/lib/home_page.dart
+++ instagram_clone/lib/home_page.dart
@@ -26,7 +26,39 @@ class HomePage extends StatelessWidget {
               children: <Widget>[
                 Text('Instagram에 오신 것을 환영합니다.', style: TextStyle(fontSize: 24.0),
                 ),
+                Padding(padding: EdgeInsets.all(8.0),
+                ),
                 Text('사진과 동영상을 보려면 팔로우 하세요',
+                ),
+                SizedBox(
+                  width: 260.0,
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 80.0,
+                          height: 80.0,
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage('http://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2018/02/02/Y8qABqMeFbhH636531733604162498.jpg'),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.all(8.0),
+                        ),
+                        Text('Email address', style:TextStyle(fontWeight: FontWeight.bold)),
+                        Text('Name'),
+                        Padding(padding: EdgeInsets.all(8.0),
+                        ),
+                        RaisedButton(
+                          child: Text('Follow'),
+                          color: Colors.blueAccent,
+                          textColor: Colors.white,
+                          onPressed: () {
+                            
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
