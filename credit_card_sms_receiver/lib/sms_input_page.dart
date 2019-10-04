a9751e17fedd6d2b3d18569270ff654b8fe95fdf
ingki3 <ingki3@gmail.com>
2019-10-04 17:57:44 +0900

    20191004_2

--- credit_card_sms_receiver/lib/sms_input_page.dart
+++ credit_card_sms_receiver/lib/sms_input_page.dart
@@ -6,6 +6,30 @@ class SmsInputPage extends StatefulWidget {
 }
 
 class _SmsInputPageState extends State<SmsInputPage> {
+  final _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener((){
+      final text = _controller.text;
+      _controller.value = _controller.value.copyWith(
+        text: text,
+        selection: TextSelection(
+            baseOffset: text.length,
+            extentOffset: text.length
+        ),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,37 +39,75 @@ class _SmsInputPageState extends State<SmsInputPage> {
           );
         }
   Widget _buildBody() {
-    return SingleChildScrollView(
+    return SafeArea(
       child: Column(
-
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(8.0),
-          ),
-          Text('Input SMS Mesaage',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+          Container(
+            color: Colors.brown,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
+          Container(
+            color: Colors.cyan,
+            child: Text('Input SMS Mesaage',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
+          Container(
+            color: Colors.brown,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
+          Expanded(
+            child: Container(
+              color: Colors.blueGrey,
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                controller: _controller,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              ),
+            ),
           ),
-          MaterialButton(
-            child: Text('Input SMS', style: TextStyle(color: Colors.white),),
-            color: Colors.blue,
-            height: 48.0,
-            onPressed: () {},
+          Container(
+            color: Colors.brown,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
+          ),
+          Container(
+            color: Colors.brown,
+            child: RaisedButton(
+              child: Text('Input SMS', style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: () {
+                _parseString();
+              },
+            ),
+          ),
+          Container(
+            color: Colors.brown,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+            ),
           ),
         ],
       ),
     );
+  }
+  void _parseString() {
+    _parseBCCard(_controller.text);
+  }
+
+  void _parseBCCard(String str) {
+    List<String> splitedStr = str.split('\n');
+    for(var n in splitedStr) {
+      print(n);
+    }
   }
 }
